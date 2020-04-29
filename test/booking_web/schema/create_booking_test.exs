5861cc86d28d6ac5b250fda2a7def2f6a192fcff
Mattias Lundberg <me@mlundberg.se>
2020-04-29 18:38:19 +0200

    Add tests for create booking errors

--- test/booking_web/schema/create_booking_test.exs
+++ test/booking_web/schema/create_booking_test.exs
@@ -2,6 +2,51 @@ defmodule BookingWeb.CreateBookingTest do
   use BookingWeb.ConnCase
   alias Booking.{Location, Repo, Bookable}
 
+  test "missing bookable" do
+    now = DateTime.utc_now()
+
+    query = """
+    mutation {
+      createBooking(label:"From graph" start:"#{now}" end:"#{now}") {
+        label id
+      }
+    }
+    """
+
+    assert {:ok,
+            %{
+              errors: [
+                %{
+                  message: "In argument \"bookableId\": Expected type \"Int!\", found null."
+                }
+              ]
+            }} = Absinthe.run(query, Booking.Schema)
+  end
+
+  test "invalid date" do
+    location = %Location{} |> Location.changeset(%{name: "First"}) |> Repo.insert!()
+    {:ok, bookable} = Bookable.create(nil, %{location_id: location.id, name: "Bookable"}, nil)
+
+    now = DateTime.utc_now()
+
+    query = """
+    mutation {
+      createBooking(bookableId: #{bookable.id}, label:"From graph", start:"notdate" end:"#{now}") {
+        label id
+      }
+    }
+    """
+
+    assert {:ok,
+            %{
+              errors: [
+                %{
+                  message: "Argument \"start\" has invalid value \"notdate\"."
+                }
+              ]
+            }} = Absinthe.run(query, Booking.Schema)
+  end
+
   test "create successful" do
     location = %Location{} |> Location.changeset(%{name: "First"}) |> Repo.insert!()
     {:ok, bookable} = Bookable.create(nil, %{location_id: location.id, name: "Bookable"}, nil)
