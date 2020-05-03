7573752e5ce15c414cd7f37b1c4d3f190704ff6a
Mattias Lundberg <me@mlundberg.se>
2020-05-03 19:24:15 +0200

    Add shell for rendering current and next booking

--- assets/js/location.ts
+++ assets/js/location.ts
@@ -1,29 +1,41 @@
-import { clearElement, createElement } from "./dom_helpers";
-import { queryGraph } from "./http_helpers";
+import { clearElement, createElement } from './dom_helpers';
+import { queryGraph } from './http_helpers';
 
 interface Bookable {
   id: number;
   name: string;
 }
 
+const renderBookable = (parent: Element, bookable: Bookable) => {
+  const card = createElement('div', parent, { class: 'bookable-item' });
+  createElement('div', card, { class: 'label', text: bookable.name });
+
+  // if (bookable.currentBooking) {
+
+  // }
+
+  // if (bookable.nextBooking) {
+
+  // }
+};
+
 const renderBookables = (parent: Element, bookables: Bookable[]) => {
-  bookables.forEach((b) => {
-    createElement("div", parent, { text: b.name });
-  });
+  const bookableGrid = createElement('div', parent, { class: 'bookable-grid' });
+  bookables.forEach((b) => renderBookable(bookableGrid, b));
 };
 
 export const renderLocation = async (
   locationId: string,
   parent: Element,
-  token: string
+  token: string,
 ) => {
   clearElement(parent);
 
   const locationData = await queryGraph(
     token,
-    `query { location(id: ${locationId}) { name bookables { id name } } }`
+    `query { location(id: ${locationId}) { name bookables { id name } } }`,
   );
 
-  createElement("div", parent, { text: locationData.data.location.name });
+  createElement('div', parent, { text: locationData.data.location.name });
   renderBookables(parent, locationData.data.location.bookables);
 };
